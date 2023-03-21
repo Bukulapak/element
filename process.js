369f6b16ab691f1ec5f6022c964709be548d8500
Indra Riksa Herlambang <indrariksa@gmail.com>
2023-03-21 12:36:30 +0700

    Create process.js

---
+++ process.js
@@ -0,0 +1,31 @@
+export function getValue(id){
+    return document.getElementById(id).value;
+}
+
+export function setInner(id,content){
+    document.getElementById(id).innerHTML = content;
+}
+
+export function addInner(id,content){
+    document.getElementById(id).innerHTML += content;
+}
+
+export function addChild(id,tag,classvalue,content){
+    let el = document.createElement(tag);
+    let classArray = classvalue.split(" ");
+    classArray.forEach(setClassValue.bind(null,el));
+    el.innerHTML = content;
+    document.getElementById(id).appendChild(el);
+}
+
+function setClassValue(el,classvalue){
+    el.classList.add(classvalue.trim());
+}
+
+export function show(id){
+    document.getElementById(id).style.display = 'block';
+}
+
+export function hide(id){
+    document.getElementById(id).style.display = 'none';
+}
